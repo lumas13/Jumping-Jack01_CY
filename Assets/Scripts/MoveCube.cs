c8247be1f164e6abdcd0bd6c06f1d8f2815f9110
Eli <72849929+lumas13@users.noreply.github.com>
2020-11-08 13:24:11 +0800

    Initial commit

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    float speed = 1.0f;
+    float zLimit = 27.5f;
+    bool isFoward = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < zLimit && isFoward) // == true
+        {
+            Foward();
+        }
+        else if (transform.position.z > 1 && !isFoward) // == false
+        {
+            Backward();
+        }
+        else
+        {
+            isFoward = !isFoward;
+        }
+    }
+
+    private void Foward()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void Backward()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+    }
+}
---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public GameObject moveCube;
+
+    float playerSpeed = 10.0f;
+    float jumpForce = 10.0f;
+    float gravityModifier = 3.0f;
+
+    bool isGrounded;
+    bool onMoveCube;
+
+    Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        onMoveCube = false;
+
+        Physics.gravity *= gravityModifier;
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * playerSpeed);
+
+        if (onMoveCube == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, moveCube.transform.position.z);
+        }
+
+        if (onMoveCube == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * playerSpeed);
+        }
+        PlayerJump();       
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("MoveCube"))
+        {
+            isGrounded = true;
+            onMoveCube = true;
+        }
+    }
+}
+        
